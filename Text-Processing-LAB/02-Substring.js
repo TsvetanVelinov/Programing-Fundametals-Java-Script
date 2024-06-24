a145dd44c78963eef618da908e11f6083f142672
Velinov <124807884+TsvetanVelinov@users.noreply.github.com>
2024-06-24 15:56:15 +0300

    Create 02-Substring.js

---
+++ Text-Processing-LAB/02-Substring.js
@@ -0,0 +1,11 @@
+function substring(input,startSub,count) {
+    
+
+    let substring = input.substring(startSub,startSub+count);
+
+
+    console.log(substring);
+
+}
+
+substring('SkipWord', 4 ,7)  
