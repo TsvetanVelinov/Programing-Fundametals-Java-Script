e15441e6d90c6514b993159253082fccaf783c1a
Velinov <124807884+TsvetanVelinov@users.noreply.github.com>
2024-06-24 15:55:57 +0300

    Create 01-Print-Characters.js

---
+++ Text-Processing-LAB/01-Print-Characters.js
@@ -0,0 +1,14 @@
+function printCharacters(input) {
+
+    for(let index of input){
+        console.log(index);
+    }
+
+
+}
+
+printCharacters('Sentence')
+
+
+
+//Write a function that receives a string and prints all the characters on separate lines.
